a6a5369e5374947b493cd29930992d36730ffa07
Yousef Walid Abd-ElWahab <140533387+ysfw@users.noreply.github.com>
2025-08-07 22:16:20 +0300

    Add chess piece classes and board initialization logic (not working properly yet)

--- board.h
+++ board.h
@@ -16,28 +16,25 @@
 // };
 #pragma once
 #include <bits/stdc++.h>
-#include "piece.h"
+#include "pieces/piece.h"
+#include "pieces/rook.h"
+#include "pieces/bishop.h"
+#include "pieces/knight.h"
+#include "pieces/king.h"
+#include "pieces/queen.h"
+#include "pieces/pawn.h"
 using namespace std;
 
 
 
-enum alpha {
-    a=0,
-    b,
-    c,
-    d,
-    e,
-    f,
-    g,
-    h,
- };
+
 
 class board
 {
 private:
     vector<vector<piece*>> Board;
-    vector<piece> whitePieces;
-    vector<piece> blackPieces;
+    map<string,piece*> whitePieces;
+    map<string,piece*> blackPieces;
 public:
     board();
     vector<pair<int,int>> AttackedBy(pair<int,int> position); 
@@ -51,7 +48,48 @@ public:
 
 board::board ()
 :Board(8, vector<piece*>(8, nullptr)){
+    whitePieces.insert({"a1", new rook(true,{0,0})});
+    whitePieces.insert({"b1", new Knight(true,{0,1})});
+    whitePieces.insert({"c1", new bishop(true,{0,2})});
+    whitePieces.insert({"d1", new king(true,{0,3})});
+    whitePieces.insert({"e1", new queen(true,{0,4})});
+    whitePieces.insert({"f1", new bishop(true,{0,5})});
+    whitePieces.insert({"g1", new Knight(true,{0,6})});
+    whitePieces.insert({"h1", new rook(true,{0,7})});
+    for (int i = 0; i < 8; i++)    whitePieces.insert({static_cast<char>('a' + i) + "2", new pawn(true,{1,i})});
     
+    blackPieces.insert({"a8", new rook(false,{7,0})});
+    blackPieces.insert({"b8", new Knight(false,{7,1})});
+    blackPieces.insert({"c8", new bishop(false,{7,2})});
+    blackPieces.insert({"d8", new king(false,{7,3})});
+    blackPieces.insert({"e8", new queen(false,{7,4})});
+    blackPieces.insert({"f8", new bishop(false,{7,5})});
+    blackPieces.insert({"g8", new Knight(false,{7,6})});
+    blackPieces.insert({"h8", new rook(false,{7,7})});
+    for (int i = 0; i < 8; i++)    blackPieces.insert({static_cast<char>('a' + i) + "7", new pawn(false,{6,i})});
+    
+    for(int i=0; i < 8; i++){
+        string pos1 = string(1, static_cast<char>('a' + i)) + "1";
+        int firstcoord = whitePieces[pos1]->getPosition().first;
+        int secondcoord = whitePieces[pos1]->getPosition().second;
+        Board[static_cast<size_t>(firstcoord)][static_cast<size_t>(secondcoord)] = whitePieces[pos1];
+
+        string pos2 = string(1, static_cast<char>('a' + i)) + "2";
+        firstcoord = whitePieces[pos2]->getPosition().first;
+        secondcoord = whitePieces[pos2]->getPosition().second;
+        Board[static_cast<size_t>(firstcoord)][static_cast<size_t>(secondcoord)] = whitePieces[pos2];
+
+        string pos8 = string(1, static_cast<char>('a' + i)) + "8";
+        firstcoord = blackPieces[pos8]->getPosition().first;
+        secondcoord = blackPieces[pos8]->getPosition().second;
+        Board[static_cast<size_t>(firstcoord)][static_cast<size_t>(secondcoord)] = blackPieces[pos8];
+
+        string pos7 = string(1, static_cast<char>('a' + i)) + "7";
+        firstcoord = blackPieces[pos7]->getPosition().first;
+        secondcoord = blackPieces[pos7]->getPosition().second;
+        Board[static_cast<size_t>(firstcoord)][static_cast<size_t>(secondcoord)] = blackPieces[pos7];
+    }
+
 }
 
 piece* board :: getAt(pair<int,int> position){
@@ -69,8 +107,8 @@ void board :: printBoardB (){
         cout<< i+1<<' ';
         for (size_t j = 0; j < 8; j++)
         {
-            cout<<Board[i][j]->getValue()<<' ';
-            
+            if (Board[i][j] == nullptr ) (i+j) % 2 == 0 ? cout<<"■" <<' ': cout<<"□"<<' ';
+            else cout<<Board[i][j]->getValue()<<' ';
         }
         cout<<'\n';
     }
@@ -87,7 +125,8 @@ void board :: printBoardW (){
         cout<< i+1<<' ';
         for (int j = 7; j >= 0; j--)
         {
-            cout<<Board[i][j]->getValue()<<' ';
+            if (Board[i][j] == nullptr ) (i+j) % 2 == 0 ? cout<<"■" <<' ': cout<<"□"<<' ';
+            else cout<<Board[i][j]->getValue()<<' ';
             
         }
         cout<<'\n';
--- pieces/piece.h
+++ pieces/piece.h
@@ -1,27 +1,38 @@
 #pragma once
 #include <bits/stdc++.h>
-#include "board.h"
+#include "../board.h"
 using namespace std;
 class piece
 {
 private:
     pair<int, int> position;
-    vector<pair<int, int>> possibleMoves;
     bool White;
     string value ;
+    /// @brief 
+    vector<pair<int, int>> possibleMoves;
 
 public:
-    piece(bool White, pair<int, int> startingPosition);
+    piece();
+    piece(bool isWhite, pair<int, int> startingPosition);
     virtual ~piece() {}
     bool isWhite(){return White;};
     void updatePos(pair<int, int> newPosition);
+    pair<int, int> getPosition();
     string getValue();
     void setValue(string);
     void addPossibleMove(pair<int, int> move);
+
 };
-piece :: piece (bool White, pair<int, int> startingPosition){
-    this->White = White;
-    this->position = startingPosition;
+
+piece :: piece (bool white, pair<int, int> startingPosition)
+    : position(startingPosition), White(white), value(""), possibleMoves() {}
+
+piece::piece()
+    : position({-1, -1}), White(false), value(""), possibleMoves() {}
+
+pair<int, int> piece::getPosition()
+{
+    return position;
 }
 
 void piece::updatePos(pair<int, int> newPosition)
@@ -41,11 +52,3 @@ void piece:: addPossibleMove(pair<int, int> move){
     possibleMoves.push_back(move);
 }
 
-
-
-
-class emptySquare : piece
-{
-public:
-    emptySquare(bool White, pair<int, int> position);
-};
